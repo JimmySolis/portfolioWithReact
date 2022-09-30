1f3647a2504ed3d1a386e4f6c861f3e1827fe9bc
Jimmy Solis <jimmysolis14@gmail.com>
2022-09-30 16:16:48 -0400

    I have made the bare bones of my project. As of now there is very little to it but ill start to add things.

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Contact () {
+    return (
+        <div>
+            <h1> Contact Page</h1>
+            <p>
+                If you would like to reach me please contact me by email: jimmysolis14@gmail.com
+                To see all my repositories here is my username for Github: JimmySolis
+                Here is my linked-in profile page: www.linkedin.com/in/jimmysoliscoding
+            </p>
+        </div>
+    );
+}
